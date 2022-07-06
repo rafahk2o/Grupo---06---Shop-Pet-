3e10eead77307d81673e6e8e093bba891bfff31b
Nicolas Rodrigues Moura <108817259+Nicolas-RMoura@users.noreply.github.com>
2022-07-06 17:30:43 -0300

    Iniciando a Home(Cabeçalho)

---
+++ Grupo-06-Shop Pet/js/script.js
@@ -0,0 +1,26 @@
+// Barra de pesquisa
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+
+// Carrinho de compra
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+//Formulário do login
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+}
