c2f28aad089338827f150ba6ae655988c565eed7
Rafael Silva Dev <49276128+rafahk2o@users.noreply.github.com>
2022-09-15 15:10:07 -0300

    menu responsivo

--- js/script.js
+++ js/script.js
@@ -35,3 +35,54 @@ boletoRadio.addEventListener("click", () => {
   cardPag.checked = false;
   pixPag.checked = false;
 });
+
+//Barra de pesquisa
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+
+//Carrinho de compra
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+//Formulário do login
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+}
+
+//Menu responsivo do cabeçalho
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  cart.classList.remove('active');
+  searchForm.classList.remove('active');
+  loginForm.classList.remove('active');
+});
+
+//Banner Rotativo
+let banner = 1;
+setInterval(function(){
+    document.getElementById('radio' + banner).checked = true;
+    banner++;
+    if(banner > 2){
+        banner = 1;
+    }
+}, 5000);
+
