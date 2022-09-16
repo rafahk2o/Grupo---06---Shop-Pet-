8e53e9fdb19f118afee90db7278d3aa29013110d
Renan da Silva Reis <82418573+Sudo-Renan@users.noreply.github.com>
2022-09-16 14:56:55 -0300

    Add files via upload

--- js/adocao.js
+++ js/adocao.js
@@ -1,45 +1,4 @@
-//Barra de pesquisa
-let searchForm = document.querySelector('.search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    cart.classList.remove('active');
-    loginForm.classList.remove('active');
-};
-
-//Carrinho de compra
-let cart = document.querySelector('.shopping-cart');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cart.classList.toggle('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-}
-
-//Formulário do login
-let loginForm = document.querySelector('.login-form');
-
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cart.classList.remove('active');
-}
-
-//Banner Rotativo
-let banner = 1;
-setInterval(function(){
-    document.getElementById('radio' + banner).checked = true;
-    banner++;
-    if(banner > 2){
-        banner = 1;
-    }
-}, 5000);
-
-
-
-
 //Animacao no scroll
-
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
@@ -60,5 +19,3 @@ function reveal() {
 
   // To check the scroll position on page load
   reveal();   
-
-  // Para verificar a posição de rolagem no carregamento da página
